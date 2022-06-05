c637ed775308c178f8eb5b52d326beb10cc265be
saurabhshete100X <saurabhshete281@gmail.com>
2022-06-05 23:17:15 +0530

    Mongo 1st Assignment Done

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 
-mongoose.connect("mongodb+srv://functionup-cohort:G0Loxqc9wFEGyEeJ@cluster0.rzotr.mongodb.net/Pritesh87698-DB?retryWrites=true&w=majority", {
+mongoose.connect("mongodb+srv://saurabhshete281:JBmRPjzC58VOejIX@cluster0.jih1tjw.mongodb.net/saurabh-db", {
     useNewUrlParser: true
 })
 .then( () => console.log("MongoDb is connected"))
