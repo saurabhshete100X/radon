c9ae4194bf56d7f90d92dd738d249806e1674c18
saurabhshete100X <saurabhshete281@gmail.com>
2022-06-27 11:02:46 +0530

    Project Done

--- src/controller/authorController.js
+++ src/controller/authorController.js
@@ -2,7 +2,7 @@ const jwt = require("jsonwebtoken");
 
 const authorModel = require("../models/authorModel")
 
-const createAuthor = async function(req, res) {
+const createAuthor = async function (req, res) {
     try {
         let details = req.body;
 
@@ -22,15 +22,15 @@ const createAuthor = async function(req, res) {
         res.status(500).send({ status: false, error: err.message });
     }
 }
-const loginUser = async function(req, res) {
+const loginUser = async function (req, res) {
     try {
         let email = req.body.email
         let password = req.body.password
         let user = await authorModel.findOne({ email: email, password: password })
         if (!user) return res.status(404).send({ status: false, Error: "Invalid email or password" })
         let token = jwt.sign({
-                userId: user._id.toString(),
-            },
+            userId: user._id.toString(),
+        },
             "functionUp-radon"
         );
         res.setHeader("x-api-key", token);
